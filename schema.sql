4dc1b97b0aed639898b53f8a0defde92b531e91e
ds20220914 <davidkuul2002@gmail.com>
2023-10-03 22:19:52 +0300

    added requirements.txt ja schema.sql

---
+++ schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    password TEXT,
+);
+
+
+CREATE TABLE School (
+    id SERIAL PRIMARY KEY,
+    Blog_name TEXT,
+    Blog_text TEXT,
+);
+
+CREATE TABLE Life (
+    id SERIAL PRIMARY KEY,
+    Blog_name TEXT,
+    Blog_text TEXT,
+);
+
+
+CREATE TABLE Sport (
+    id SERIAL PRIMARY KEY,
+    Blog_name TEXT,
+    Blog_text TEXT,
+);
+
+CREATE TABLE Game (
+    id SERIAL PRIMARY KEY,
+    Blog_name TEXT,
+    Blog_text TEXT,
+);
