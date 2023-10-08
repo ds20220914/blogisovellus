e0bdb48fad80459cae69b847a9a589b113b3c752
ds20220914 <davidkuul2002@gmail.com>
2023-10-08 23:27:29 +0300

    new table for comments and new page for writing comments

--- schema.sql
+++ schema.sql
@@ -28,3 +28,29 @@ CREATE TABLE Game (
     Blog_name TEXT,
     Blog_text TEXT
 );
+
+CREATE TABLE School_comment (
+    id SERIAL PRIMARY KEY,
+    Blog_id TEXT,
+    content TEXT
+);
+
+CREATE TABLE life_comment (
+    id SERIAL PRIMARY KEY,
+    Blog_id TEXT,
+    content TEXT
+);
+
+CREATE TABLE sport_comment (
+    id SERIAL PRIMARY KEY,
+    Blog_id TEXT,
+    content TEXT
+);
+
+CREATE TABLE game_comment (
+    id SERIAL PRIMARY KEY,
+    Blog_id TEXT,
+    content TEXT
+);
+
+
