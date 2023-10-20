43684ef1f2f4dfe601a8dd7c6a297bc94a11f040
ds20220914 <davidkuul2002@gmail.com>
2023-10-20 10:53:53 +0300

    pylint change

--- blogs.py
+++ blogs.py
@@ -5,14 +5,18 @@ from db import db
 
 
 def find_all_blogs(community):
-    """find all blogs by given community name"""
+    """find all blogs by given community name
+       Args: community name
+       returns: all the blogs that found"""
     query = text("SELECT blog_name,id FROM Blogs WHERE community=:community")
     right_blogs = db.session.execute(query,{"community":community})
     blogs=right_blogs.fetchall()
     return blogs
 
 def find_userid_by_name(name):
-    """find the user's id by given name"""
+    """find the user's id by given name
+       Args: username
+       returns: user's id"""
     query = text("SELECT id FROM users WHERE username=:name")
     right_id = db.session.execute(query,{"name":name})
     id1=right_id.fetchall()
@@ -20,7 +24,8 @@ def find_userid_by_name(name):
 
 def create_blog(topic,content,writer,community):
     """create a blog using given information of the topic,content,
-       the writer name and community of the blog."""
+       the writer name and community of the blog.
+       Args: blog topic, content, writer's name and community name"""
     sql =text( "INSERT INTO Blogs (blog_name, content,time,user_id,community) VALUES"
                 "(:topic, :content,NOW(),:writer,:community)")
     db.session.execute(sql,
@@ -30,14 +35,18 @@ def create_blog(topic,content,writer,community):
 
 def find_text(community,blog_id ):
     """find the content text of a blog using given community name and the blog's
-       id"""
+       id
+       Args: community name, blog's id
+       returns: the blog's text"""
     query = text("SELECT content FROM Blogs WHERE id=:id AND community=:community")
     right_blogs = db.session.execute(query,{"id":blog_id,"community":community})
     blog=right_blogs.fetchone()
     return blog
 
 def find_all_blogs_byname(name,community):
-    """find all the blogs that has the same name in one community"""
+    """find all the blogs that has the same name in one community
+       Args: blog's name, community name
+       returns: blog's name and id"""
     query = text("SELECT blog_name,id FROM Blogs"
                  " WHERE Blog_name=:Blog_name AND community=:community")
     right_blogs = db.session.execute(query,{"Blog_name":name, "community":community})
@@ -45,20 +54,26 @@ def find_all_blogs_byname(name,community):
     return blogs
 
 def add_comment(id1,content):
-    """add a comment to the blog"""
+    """add a comment to the blog
+       Args: blog's id and comment text
+       """
     query = text(" INSERT INTO Comments (Blog_id,content) VALUES (:id , :content)")
     db.session.execute(query,{"id":id1, "content":content})
     db.session.commit()
 
 def find_comments(blog_id):
-    """ find all the comments of a blog"""
+    """ find all the comments of a blog
+        Args: blog's id
+        returns: all the comments that found"""
     query = text(" SELECT content FROM Comments WHERE Blog_id=:id ")
     right_comments = db.session.execute(query,{"id":blog_id})
     comments=right_comments.fetchall()
     return comments
 
 def all_my_blogs(username):
-    """ find all the blogs that user has posted"""
+    """ find all the blogs that user has posted
+        Args:username
+        returns: all the blogs user posted"""
     list1=[]
     query1 = text("SELECT * FROM Blogs WHERE user_id=:writer AND community=:school")
     right_blogs1 = db.session.execute(query1,{"writer":username,"school":"school"})
@@ -90,7 +105,9 @@ def all_my_blogs(username):
         list1.append(blogs4)
     return list1
 def delete_blog(id1):
-    """ delete one single blog using given blog's id"""
+    """ delete one single blog using given blog's id
+        Args: blog's id
+        """
     for i in id1:
         query = text(" DELETE FROM Blogs WHERE id=:i ")
         db.session.execute(query,{"i":i})
@@ -98,7 +115,8 @@ def delete_blog(id1):
 
 def find_all_all_blogs():
     """find all the blogs that in the app and divide into
-       different groups according to their community"""
+       different groups according to their community
+       returns: all the blogs that in this app"""
     list1=[]
     query1 = text("SELECT * FROM Blogs WHERE community=:school")
     right_blogs1 = db.session.execute(query1,{"school":"school"})
@@ -118,34 +136,44 @@ def find_all_all_blogs():
     list1.append(blogs4)
     return list1
 def check_if_admin(user_id):
-    """check if this user is admin"""
+    """check if this user is admin
+       Args: user's id
+       returns: if not admin then none or if admin then admin id"""
     query=text("SELECT id FROM Admin WHERE admin_id=:id ")
     admin=db.session.execute(query,{"id":user_id[0]})
     admin_id=admin.fetchall()
     return admin_id
 
 def recently_added_blog(user):
-    """find the user's latest added blog"""
+    """find the user's latest added blog
+       Args: username
+       returns: the blog's id"""
     query1=text("SELECT MAX(id) FROM Blogs WHERE user_id=:id ")
     right_blogs1 = db.session.execute(query1,{"id":user})
     id1=right_blogs1.fetchone()
     return id1
 
 def add_blog_password(blogid,password):
-    """add a private password to one single blog"""
+    """add a private password to one single blog
+       Args: blog's id and password
+       """
     query1 = text("INSERT INTO Private_blogs (private_blog_id,password) VALUES (:id,:password) ")
     db.session.execute(query1,{"id":blogid, "password":password})
     db.session.commit()
 
 def check_if_blog_password(blog_id):
-    """check if a blog is private and has password"""
+    """check if a blog is private and has password
+       Args:blog's id
+       Returns: password if there is"""
     query1 = text("SELECT password FROM Private_blogs WHERE private_blog_id=:id ")
     right_blogs1 = db.session.execute(query1,{"id":blog_id})
     password=right_blogs1.fetchone()
     return password
 
 def find_writer_name(blogid):
-    """ find the name of blog writer"""
+    """ find the name of blog writer
+        Args: blog's id
+        returns: writer name"""
     query1 = text("SELECT U.username FROM Blogs B LEFT JOIN Users U"
                    " ON B.user_id=U.id WHERE B.id=:id")
     right_name = db.session.execute(query1,{"id":blogid})
--- routes.py
+++ routes.py
@@ -1,86 +1,108 @@
-from app import app
+"""here are all the passes between the pages"""
 from flask import redirect, render_template,request,session
-import user
 from flask_sqlalchemy import SQLAlchemy
+from app import app
+import user
 import blogs
 
 
 @app.route("/")
 def index():
+    """
+       This route renders and displays the start page to the user.
+       returns:Rendered templates"""
     return render_template("start.html")
 
 @app.route("/logout")
 def logout():
+    """ This route delete the user's login session,
+        and displays the start page to the user.
+        returns:Rendered templates"""
     del session["username"]
     return redirect("/")
 
 @app.route("/result", methods=["POST"])
 def result():
+    """Process the form data and handle user sign-in or new user creation.
+       returns: redirects to start route."""
     username1 = request.form["username"]
     password1 = request.form["password"]
     button_pressed = request.form.get("button")
     if button_pressed == "create new user":
         return render_template("new_user.html")
     if button_pressed == "sign in":
-        result=user.login(username1,password1)
-        if result==False:
+        result1=user.login(username1,password1)
+        if result1 is False:
             return render_template("error.html",message="username or password wrong, try again")
-        message=""
         session["username"]=username1
         session["community"]=""
         return redirect("/")
 
 @app.route("/new_user", methods=["POST"])
 def new_user():
+    """Handle the creation of a new user.
+       returns:rendered templates or redirect to start page"""
     new_username=request.form["new username"]
     new_password=request.form["new password"]
     right=user.new_user(new_username,new_password)
-    if right==True:
+    if right is True:
         return redirect("/")
-    if right==False:
-        return render_template("error2.html",message="username is already exist or password is too short")
+    if right is False:
+        return render_template("error2.html",
+                                message="username is already exist or password is too short")
 
 @app.route("/Blog",methods=["POST","GET"])
 def Blog():
-     if request.method=="POST":
-         community = request.form["community"]
-         session["community"]=community
-     community=session["community"]
-     list=blogs.find_all_blogs(session["community"])
-     return render_template("Blog.html",community=community,list=list)
+    """show all the blogs in a community
+       returns: Rendered templates"""
+    if request.method=="POST":
+        community = request.form["community"]
+        session["community"]=community
+    community=session["community"]
+    list1=blogs.find_all_blogs(session["community"])
+    return render_template("Blog.html",community=community,list1=list1)
 
 @app.route("/Blog2",methods=["POST","GET"])
 def Blog2():
+    """show single blog
+       returns: Rendered templates"""
     button= request.args.get("find")
     community = session["community"]
     if button=="search":
         blog_name = request.args.get('query')
-        if blog_name==None or len(blog_name)==0:
-            list=blogs.find_all_blogs(community)
-            return render_template("Blog.html",community=community,list=list)
-        list=blogs.find_all_blogs_byname(blog_name,community)
-        return render_template("Blog.html",community=community,list=list)
+        if blog_name is None or len(blog_name)==0:
+            list1=blogs.find_all_blogs(community)
+            return render_template("Blog.html",community=community,list1=list1)
+        list1=blogs.find_all_blogs_byname(blog_name,community)
+        return render_template("Blog.html",community=community,list1=list1)
 
-    else:
-        community=request.args.get('community')
-        if session["community"]=="":
-            session["community"]=community
+    community=request.args.get('community')
+    if session["community"]=="":
+        session["community"]=community
 
-        blog_id = request.args.get('blog_id')
-        message=blogs.find_text(community,blog_id)
-        blog_name=request.args.get('blog_name')
-        comments=blogs.find_comments(blog_id)
-        blog_password=blogs.check_if_blog_password(blog_id)
-        writer_name=blogs.find_writer_name(blog_id)
-        if blog_password!=None:
-            if writer_name[0]!=session["username"] and session["username"]!="admin1":
-                return render_template("private.html",passw=blog_password[0],blog_id=blog_id,community=community,blog_name=blog_name,message=message[0])
+    blog_id = request.args.get('blog_id')
+    message=blogs.find_text(community,blog_id)
+    blog_name=request.args.get('blog_name')
+    comments=blogs.find_comments(blog_id)
+    blog_password=blogs.check_if_blog_password(blog_id)
+    writer_name=blogs.find_writer_name(blog_id)
+    if blog_password is not None:
+        if writer_name[0]!=session["username"] and session["username"]!="admin1":
+            return render_template("private.html",
+                                    passw=blog_password[0],blog_id=blog_id,
+                                    community=community,blog_name=blog_name,
+                                    message=message[0])
 
-        return render_template("Blog2.html",blog_id=blog_id,community=community,blog_name=blog_name,message=message[0],comments=comments)
+    return render_template("Blog2.html",
+                            blog_id=blog_id,community=community,
+                            blog_name=blog_name,message=message[0],
+                            comments=comments)
 
 
 @app.route("/check_password",methods=["POST"])
 def check_password():
+    """check if a blog has password
+       returns: Rendered template"""
     password=request.form["password"]
     password1=request.form["password1"]
     blog_id=request.form["blog_id"]
@@ -89,21 +111,28 @@ def check_password():
     message=request.form["message"]
     if password==password1:
         comments=blogs.find_comments(blog_id)
-        return render_template("Blog2.html",blog_id=blog_id,community=community,blog_name=blog_name,message=message,comments=comments)
+        return render_template("Blog2.html",blog_id=blog_id,community=community,
+                                blog_name=blog_name,message=message,comments=comments)
 
 @app.route("/new_blog")
 def new_blog():
+    """show the page where user can write new blog
+       returns: Rendered template"""
     return render_template("new.html")
 
 @app.route("/create", methods=["POST"])
 def create():
+    """create a new blog and go back to start page
+       returns: rendered templates"""
     topic = request.form["topic"]
     community=request.form["community"]
     content=request.form["content"]
     if topic=="" or content=="" or community=="":
-        return render_template("error3.html",message="topic,community and blog content cannot be blank, write something into the textbox.")
-    user=session["username"]
-    userid=blogs.find_userid_by_name(user)
+        return render_template("error3.html",
+           message="topic,community and blog content cannot be blank,"
+                    "write something into the box.")
+    user1=session["username"]
+    userid=blogs.find_userid_by_name(user1)
     blog_password=request.form["password"]
     blogs.create_blog(topic,content,int(userid[0]),community)
     blog_id=blogs.recently_added_blog(userid[0])
@@ -114,13 +143,18 @@ def create():
 
 @app.route("/add_comment", methods=["POST","GET"])
 def add_comment():
+    """show add a comment page
+       returns:rendered template"""
     community=session["community"]
     blog_id=request.args.get("blog_id")
     blog_name=request.args.get("blog_name")
-    return render_template("new_comment.html",community=community,blog_id=blog_id,blog_name=blog_name)
+    return render_template("new_comment.html",
+                            community=community,blog_id=blog_id,blog_name=blog_name)
 
 @app.route("/add_comment2", methods=["POST","GET"])
 def add_comment2():
+    """add a comment can go back to list page
+       returns:rendered template"""
     community=session["community"]
     blog_id=request.form["blog_id"]
     content=request.form["query"]
@@ -128,10 +162,14 @@ def add_comment2():
     blogs.add_comment(blog_id,content)
     message=blogs.find_text(community,blog_id)
     comments=blogs.find_comments(blog_id)
-    return render_template("Blog2.html",community=community,blog_id=blog_id,blog_name=blog_name,message=message[0],comments=comments)
+    return render_template("Blog2.html",
+                            community=community,blog_id=blog_id,
+                            blog_name=blog_name,message=message[0],comments=comments)
 
 @app.route("/all_my_blogs",methods=["POST","GET"])
 def all_my_blogs():
+    """find all the blogs user has posted
+       returns:rendered templates"""
     if request.method=="POST":
         delete=request.form.getlist("id")
         blogs.delete_blog(delete)
@@ -140,12 +178,13 @@ def all_my_blogs():
     commu3="sport"
     commu4="game"
     username=session["username"]
-    id=blogs.find_userid_by_name(username)
-    admin=blogs.check_if_admin(id)
+    id1=blogs.find_userid_by_name(username)
+    admin=blogs.check_if_admin(id1)
     if len(admin)!=0:
         list=blogs.find_all_all_blogs()
-        return render_template("all_my_blogs.html",list=list,commu1=commu1,commu2=commu2,commu3=commu3,commu4=commu4)
+        return render_template("all_my_blogs.html",list=list,commu1=commu1,
+                                commu2=commu2,commu3=commu3,commu4=commu4)
     userid=blogs.find_userid_by_name(username)
     list=blogs.all_my_blogs(userid[0])
-    return render_template("all_my_blogs.html",list=list,commu1=commu1,commu2=commu2,commu3=commu3,commu4=commu4)
-    
+    return render_template("all_my_blogs.html",list=list,commu1=commu1,
+                            commu2=commu2,commu3=commu3,commu4=commu4)
